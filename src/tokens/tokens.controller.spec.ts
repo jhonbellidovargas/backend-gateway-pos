380a418972cc4d61e83d889ec38cf54356069a2b
Jhon Bellido <jhonbellidovargas@gmail.com>
2024-03-16 22:16:20 -0500

    Add dependencies and providers to TokensController spec

--- src/tokens/tokens.controller.spec.ts
+++ src/tokens/tokens.controller.spec.ts
@@ -1,6 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TokensController } from './tokens.controller';
 import { TokensService } from './tokens.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { JwtService } from '@nestjs/jwt';
+import { CACHE_MANAGER, CacheModule } from '@nestjs/common';
 
 describe('TokensController', () => {
   let controller: TokensController;
@@ -8,7 +11,19 @@ describe('TokensController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TokensController],
-      providers: [TokensService],
+      providers: [
+        TokensService,
+        {
+          provide: getModelToken('Token'),
+          useValue: {},
+        },
+        JwtService,
+        {
+          provide: CACHE_MANAGER,
+          useValue: {},
+        },
+      ],
+      imports: [CacheModule.register({})],
     }).compile();
 
     controller = module.get<TokensController>(TokensController);
